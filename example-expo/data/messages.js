d77ca518ad19a61cb1eb6909f0c003b6ff9374bb
Pratik Vaidya <psvaidya@umich.edu>
2020-02-07 20:58:40 -0500

    initial commit

---
+++ example-expo/data/messages.js
@@ -0,0 +1,19 @@
+export default [{
+        _id: 2,
+        text: 'You can ask us for the best route to any location near the UM campus.',
+        createdAt: new Date(),
+        user: {
+            _id: 2,
+            name: 'Magicker Bus',
+        },
+    },
+    {
+        _id: 1,
+        text: 'Welcome to Magicker Bus! Let us know how we can help.',
+        createdAt: new Date(),
+        user: {
+            _id: 2,
+            name: 'Magicker Bus',
+        },
+    }
+]
